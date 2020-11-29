e107613447fde8b61a6ceaa33d5ff332cc852f72
Hubert Stefanski <hstefans@redhat.com>
2020-11-29 21:56:30 +0000

    add initial code for firebase user registration

--- app/src/main/java/com/hstefans/strap_android/activities/registrationActivity.kt
+++ app/src/main/java/com/hstefans/strap_android/activities/registrationActivity.kt
@@ -3,8 +3,10 @@
 package com.hstefans.strap_android.activities
 
 import android.R.attr.password
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.hstefans.strap_android.R
+import com.hstefans.strap_android.handlers.AuthHandler
 
 
 class registrationActivity : AppCompatActivity() {
@@ -19,36 +22,47 @@ class registrationActivity : AppCompatActivity() {
 
 
     // TODO Implement me
-//    lateinit var email: EditText
-//    lateinit var password: EditText
-//    lateinit var confirmPassword: EditText
-//    lateinit var phoneNo: EditText
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var confirmPassword: EditText
+    lateinit var phoneNo: EditText
+    lateinit var registerButton: Button
+    private lateinit var authHandler: AuthHandler
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         Log.v(TAG, "Starting Logging");
-        initView()
+
+        // Initialize view components
+        email = findViewById(R.id.registerEmailText)
+        password = findViewById(R.id.registerPasswordText)
+        confirmPassword = findViewById(R.id.registerConfirmPasswordText)
+        phoneNo = findViewById(R.id.registerPhoneNoText)
+        registerButton = findViewById(R.id.registerButton)
+
 
         auth = FirebaseAuth.getInstance()
 
+        registerButton.setOnClickListener() {
+
+            auth.createUserWithEmailAndPassword(email.toString(), authHandler.hashString(password.toString()))
+                .addOnCompleteListener(this, OnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
+                    }
+                })
+        }
+
 
     }
-    //TODO handle the registration action
-//    fun onRegister(){
-//     validateData & ensure the passwords hash matches
-//    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
-//        if(task.isSuccessful){
-//            Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }else {
-//            Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
-//        }
-//    })
-//    }
+//TODO handle the registration action
 
     /**
      * initializes the values used by this activity
@@ -56,12 +70,10 @@ class registrationActivity : AppCompatActivity() {
      */
     fun initView() {
 //        TODO Implement me
-//        email = findViewById(R.id.registerEmailText)
-//        password = findViewById(R.id.registerPasswordText)
-//        confirmPassword = FindViewById(R.id.registerConfirmPasswordText)
-//        phoneNo = FindViewbyId(r.id.registerPhoneNoText)
+
     }
-        fun validateData() {
+
+    fun validateData() {
 //            TODO Implement me
 //            function to ensure data is a valid length and type, username is unique and that password & confirmPassword match i.e generate the same hash
 
