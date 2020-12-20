d8398bbe93ca68bed5cbe29a290c2a87bd28489a
Hubert Stefanski <hstefans@redhat.com>
2020-12-20 23:01:09 +0000

    add initial map support
    fix gitignore
    
    Signed-off-by: Hubert Stefanski <hstefans@redhat.com>
    
    remove file
    
    remove file
    
    remove comment

--- app/src/main/java/com/hstefans/strap_android/handlers/TabHandler.kt
+++ app/src/main/java/com/hstefans/strap_android/handlers/TabHandler.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.hstefans.strap_android.fragments.MapsFragment
 import com.hstefans.strap_android.fragments.TaskFragment
 import com.hstefans.strap_android.fragments.UserSettingsFragment
 import com.hstefans.strap_android.fragments.WaypointsFragment
@@ -12,7 +13,7 @@ import com.hstefans.strap_android.fragments.WaypointsFragment
 internal class MyAdapter(
     var context: Context,
     fm: FragmentManager,
-    var totalTabs: Int
+    private var totalTabs: Int
 ) :
     FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
@@ -24,7 +25,7 @@ internal class MyAdapter(
                 TaskFragment()
             }
             2 -> {
-                WaypointsFragment()
+                MapsFragment()
             }
             3 -> {
                 com.hstefans.strap_android.fragments.ReportFragment()
