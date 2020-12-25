f71f94358c75b6802466addf5683e18b4d01ca35
Hubert Stefanski <hstefans@redhat.com>
2020-12-25 19:41:58 +0000

    Minor fixes, enable return to auth screen after success registration

--- app/src/test/java/com/hstefans/strap_android/RegistrationTest.kt
+++ app/src/test/java/com/hstefans/strap_android/RegistrationTest.kt
@@ -1,26 +1,11 @@
 package com.hstefans.strap_android
 
-import android.R
-import android.app.Activity
-import android.content.Intent
-import android.widget.EditText
-import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
-import com.hstefans.strap_android.activities.AuthenticationActivity
 import com.hstefans.strap_android.activities.RegistrationActivity
-import org.jetbrains.anko.find
-import org.jetbrains.anko.internals.AnkoInternals.createAnkoContext
-import org.junit.After
+
 import org.junit.Assert.*
-import org.junit.Before
+
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-import org.robolectric.shadows.ShadowToast
-import java.lang.Exception
+
 
 
 class RegistrationTest() {
